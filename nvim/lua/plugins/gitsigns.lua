a010ebc9c53bb768d60390b9340f2ad46f2f26d3
casj <casper_jellesen@hotmailcom>
2024-03-21 21:58:38 +0100

    Corrected the indent in files

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,42 +1,42 @@
 local gitsigns_config = function()
-  -- See `:help gitsigns.txt`
-  local options = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-    on_attach = function(bufnr)
-      vim.keymap.set('n', '<leader>gp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'git hunk' })
-      -- don't override the built-in and fugitive keymaps
-      local gs = package.loaded.gitsigns
-      vim.keymap.set({ 'n', 'v' }, ']c', function()
-        if vim.wo.diff then
-          return ']c'
-        end
-        vim.schedule(function()
-          gs.next_hunk()
-        end)
-        return '<Ignore>'
-      end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
-      vim.keymap.set({ 'n', 'v' }, '[c', function()
-        if vim.wo.diff then
-          return '[c'
-        end
-        vim.schedule(function()
-          gs.prev_hunk()
-        end)
-        return '<Ignore>'
-      end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
-    end,
-  }
-  require('gitsigns').setup(options)
+	-- See `:help gitsigns.txt`
+	local options = {
+		signs = {
+			add = { text = "+" },
+			change = { text = "~" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+		},
+		on_attach = function(bufnr)
+			vim.keymap.set("n", "<leader>gp", require("gitsigns").preview_hunk, { buffer = bufnr, desc = "git hunk" })
+			-- don't override the built-in and fugitive keymaps
+			local gs = package.loaded.gitsigns
+			vim.keymap.set({ "n", "v" }, "]c", function()
+				if vim.wo.diff then
+					return "]c"
+				end
+				vim.schedule(function()
+					gs.next_hunk()
+				end)
+				return "<Ignore>"
+			end, { expr = true, buffer = bufnr, desc = "Jump to next hunk" })
+			vim.keymap.set({ "n", "v" }, "[c", function()
+				if vim.wo.diff then
+					return "[c"
+				end
+				vim.schedule(function()
+					gs.prev_hunk()
+				end)
+				return "<Ignore>"
+			end, { expr = true, buffer = bufnr, desc = "Jump to previous hunk" })
+		end,
+	}
+	require("gitsigns").setup(options)
 end
 
 return {
-  -- Adds git related signs to the gutter, as well as utilities for managing changes
-  'lewis6991/gitsigns.nvim',
-  config = gitsigns_config
+	-- Adds git related signs to the gutter, as well as utilities for managing changes
+	"lewis6991/gitsigns.nvim",
+	config = gitsigns_config,
 }
