8d336e7f661cdc26c1309f13b6915b4948e82d0b
Casper Jellesen <casper_jellesen@hotmail.com>
2024-05-19 16:51:54 +0000

    Updated dotfiles

---
+++ config/hypr/hyprland_services.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo systemctl enable nvidia-suspend.service
+sudo systemctl enable nvidia-hibernate.service
+sudo systemctl enable nvidia-resume.service
+
--- hypr/auto_start_nvim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-hyprctl keyword windowrule "workspace 6 silent,^(kitty)$"
-hyprctl dispatch exec kitty nvim
-sleep 1
-hyprctl keyword windowrule "workspace unset, kitty"
