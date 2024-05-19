23b703dff6c3557737b38b4c69e3b3184a3be77a
Casper Jellesen <casper_jellesen@hotmail.com>
2024-05-19 20:47:07 +0000

    added another description with regards to nvidia on hyprland after installing the required dependencies.

--- config/hypr/hyprland_plugins.sh
+++ config/hypr/hyprland_plugins.sh
@@ -5,3 +5,6 @@ sudo pacman -S libva-nvidia-driver xdg-desktop-portal-hyprland polkit-kde-agent 
 
 # Add the following to the options list in relevant boot config file in /boot/loader/entries/ 
 # nvidia_drm.modeset=1 nvidia.NV_regPreserveVideoMemoryAllocations=1
+
+# After that add the following to the /etc/mkinitcpio.conf file
+# MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)
