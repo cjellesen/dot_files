a4961e47f10416f265dc4be84d8c3c945b627453
Casper Jellesen <casper_jellesen@hotmail.com>
2024-05-19 18:08:02 +0000

    added scripts for common language installs

--- language_installs/dotnet.sh
+++ language_installs/dotnet.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo pacman -S go
 
 wget -O dotnet-install.sh https://dot.net/v1/dotnet-install.sh
 chmod +x ./dotnet-install.sh
@@ -10,4 +9,3 @@ rm ./dotnet-install.sh
 # Add the 2 lines below to either the .bashrc or the .bash_profile depending on preference
 # export DOTNET_ROOT=$HOME/.dotnet
 # export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
-
---
+++ language_installs/pyenv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Follow the install directive at https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv for post install configuration
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+
