d9acdef11ee91f4f5c5246c236fa35979205a513
Casper Jellesen <casper_jellesen@hotmail.com>
2024-05-19 20:44:44 +0000

    added the nvidia parameters that must be added to the bootloader after install

--- config/hypr/hyprland_plugins.sh
+++ config/hypr/hyprland_plugins.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S libva-nvidia-driver xdg-desktop-portal-hyprland polkit-kde-agent qt5-wayland qt6-wayland wofi waybar pipewire wireplumber grim cliphist
+
+
+# Add the following to the options list in relevant boot config file in /boot/loader/entries/ 
+# nvidia_drm.modeset=1 nvidia.NV_regPreserveVideoMemoryAllocations=1
