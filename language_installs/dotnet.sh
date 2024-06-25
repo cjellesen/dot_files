6ea1df1ef19bed1d078690641aab33c58ebed4a7
Casper Jellesen <casper_jellesen@hotmail.com>
2024-06-25 21:18:41 +0000

    added note to dotnet.sh with regards to the Microsoft Credential provider

--- language_installs/dotnet.sh
+++ language_installs/dotnet.sh
@@ -9,3 +9,7 @@ rm ./dotnet-install.sh
 # Add the 2 lines below to either the .bashrc or the .bash_profile depending on preference
 # export DOTNET_ROOT=$HOME/.dotnet
 # export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+
+# When installing the Microsoft Credential provider add the following (depending on the location)
+# of your Nuget directory
+# export NUGET_PLUGIN_PATHS="$HOME/.nuget/plugins/netcore/CredentialProvider.Microsoft/CredentialProvider.Microsoft.dll"
