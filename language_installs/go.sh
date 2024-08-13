e209960c30fcef9dcf9d3bb6145c110289926c18
Casper <casper_jellesen@hotmail.com>
2024-08-13 08:11:55 +0200

    added vimrc

--- language_installs/go.sh
+++ language_installs/go.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 rm -rf /usr/local/go
-wget -O go_install.tar.gz https://go.dev/dl/go1.22.5.linux-386.tar.gz
+wget -O go_install.tar.gz https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
 tar -C /usr/local -xzf go_install.tar.gz
 
-# Rembember to decare a go PATH if it does not exists:
-# export PATH=$PATH:/usr/local/go/bin
-
 rm go_install.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
