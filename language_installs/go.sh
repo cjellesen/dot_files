c70c8f01cbc2aaf6a295321f3922cf7c421013c2
CASJ <casper_jellesen@hotmail.com>
2025-03-08 20:47:08 +0100

    modified the hyperland conf

--- language_installs/go.sh
+++ language_installs/go.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 rm -rf /usr/local/go
-wget -O go_install.tar.gz https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
+wget -O go_install.tar.gz https://go.dev/dl/go1.24.1.linux-amd64.tar.gz
 tar -C /usr/local -xzf go_install.tar.gz
 
 rm go_install.tar.gz
 
-export PATH=$PATH:/usr/local/go/bin
