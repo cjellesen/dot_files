925a85c86a9f897f3b6172a97a295f9f643ba40c
CASJ <casper_jellesen@hotmail.com>
2025-03-12 16:04:22 +0100

    tried to get the yamlls to work

--- config/nvim/lua/plugins/lsp/lang_format_def.lua
+++ config/nvim/lua/plugins/lsp/lang_format_def.lua
@@ -60,11 +60,36 @@ return {
 			completion = { callSnippet = "Replace" },
 		},
 	},
-
 	zls = {
 		zls = {
 			enable_autofix = true,
 			warn_style = true,
+		},
+	},
+	yamlls = {
+		settings = {
+			yaml = {
+				yamlVersion = 1.2,
+				format = {
+					enable = true,
+				},
+				validate = true,
+				hover = true,
+				completion = true,
+				schemaStore = {
+					enable = true,
+				},
+				schemas = {
+					["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/refs/heads/master/v1.32.1-standalone-strict/all.json"] = "/*.k8s.yaml",
+					["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+					["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+					["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/**/*.{yml,yaml}",
+					["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+					["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+					["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+					["http://json.schemastore.org/circleciconfig"] = ".circleci/**/*.{yml,yaml}",
+				},
+			},
 		},
 	},
 }
--- config/nvim/lua/plugins/lsp/lsp.lua
+++ config/nvim/lua/plugins/lsp/lsp.lua
@@ -216,7 +216,7 @@ return { -- LSP Configuration & Plugins
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 		require("mason-lspconfig").setup({
-			ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+			ensure_installed = {},
 			automatic_installation = false,
 			handlers = {
 				function(server_name)
