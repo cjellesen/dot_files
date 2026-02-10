9b5d9a0df1a7cecf99322a5b7ebcd5e391850287
Casper <casper_jellesen@hotmail.com>
2026-02-10 21:39:03 +0100

    small clean up

--- language_installs/go.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf /usr/local/go
-wget -O go_install.tar.gz https://go.dev/dl/go1.24.1.linux-amd64.tar.gz
-tar -C /usr/local -xzf go_install.tar.gz
-
-rm go_install.tar.gz
-
--- language_installs/pyenv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Follow the install directive at https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv for post install configuration
-curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-
