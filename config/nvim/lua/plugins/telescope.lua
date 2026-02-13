c719da5c13406c11c4801377e6c1963a152103c2
Casper <casper_jellesen@hotmail>
2026-02-13 12:38:26 +0100

    adjusted the telescope configuration according to their github instructions

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -54,8 +54,8 @@ end
 -- Fuzzy Finder (files, lsp, etc)
 return {
 	"nvim-telescope/telescope.nvim",
+    version = "*",
 	event = "VimEnter",
-	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
